dbf0c57adc8f39b120025101d1031fb1e19e49ef
Mark Holden <markholden@pplsi.com>
2024-03-13 19:17:13 -0500

    fix sorting, introduce double increment bug

--- src/BookList.tsx
+++ src/BookList.tsx
@@ -5,16 +5,40 @@ import BookListItem from './BookListItem';
 export default function BookList() {
   const [sortedBooks, sortBooks] = useState<Book[]>(books as Book[]);
 
-  const sortList = () => {
-    console.log('sortList executed');
+  const bookCompareFunction = (a: Book, b: Book) => {
+    console.log('a:', a.voteCount);
+    console.log('b:', b.voteCount);
+    
+    if (a.voteCount < b.voteCount)
+      return 1;
+    if (a.voteCount > b.voteCount)
+      return -1;
+    return 0;
+  }
+  
+  const handleUpvote = (book: Book) => {
+    console.log('handleUpvote executed');
+    sortBooks((current) => {
+      const updatedBooks = [...current];
+      updatedBooks[updatedBooks.findIndex(b => b.isbn === book.isbn)].voteCount += 1;
+      updatedBooks.sort(bookCompareFunction);
+      return updatedBooks;
+    });
+  }
 
-    sortedBooks.sort((a: Book, b: Book) => { return a.voteCount - b.voteCount; });
-    sortBooks(sortedBooks);
-  };
+  const handleDownvote = (book: Book) => {
+    console.log('handleDownvote executed');
+    sortBooks((current) => {
+      const updatedBooks = [...current];
+      updatedBooks[updatedBooks.findIndex(b => b.isbn === book.isbn)].voteCount -= 1;
+      updatedBooks.sort(bookCompareFunction);
+      return updatedBooks;
+    });
+  }
 
   return (<>
-    <ul onClick={sortList}>
-      {sortedBooks.map(b => BookListItem(b))}
+    <ul>
+      {sortedBooks.map(b => BookListItem(b, handleUpvote, handleDownvote))}
     </ul>
   </>);
 }
--- src/BookListItem.tsx
+++ src/BookListItem.tsx
@@ -1,20 +1,10 @@
-import { useState } from "react"
-
-export default function BookListItem(book: Book) {
-  const [voteCount, setVotes] = useState(book.voteCount ?? 0);
-
-  const handleUpvote = () => {
-    setVotes(voteCount + 1);
-  };
-  const handleDownvote = () => {
-    setVotes(voteCount - 1);
-  };
+export default function BookListItem(book: Book, handleUpvote: (book: Book) => void, handleDownvote: (book: Book) => void) {
 
   return (<li key={book.isbn} className="card">
     <div>
-      <button onClick={handleUpvote}><i className="fa-solid fa-arrow-up"></i></button>
-      {voteCount}
-      <button onClick={handleDownvote}><i className="fa-solid fa-arrow-down"></i></button>
+      <button onClick={() => handleUpvote(book)}><i className="fa-solid fa-arrow-up"></i></button>
+      {book.voteCount}
+      <button onClick={() => handleDownvote(book)}><i className="fa-solid fa-arrow-down"></i></button>
     </div>
     <div>
       {book.name}
